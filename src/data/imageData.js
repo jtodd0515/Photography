1243459a36fcc3ee1901bae5c852a4115524c09d
James Todd <jtodd0515@gmail.com>
2020-08-10 22:36:26 -0400

    add imageurl and image data

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -1,10 +1,37 @@
 
-export const sunsetonhatteras= [
-'/images/waterfall.jpg',
-'/images/asainstatue.jpg'
+export const sunsetHatteras= [
+    '/images/waterfall.jpg',
+    '/images/asainstatue.jpg',
+    '/images/fountain.jpg',
+    '/images/dukeduck.jpg',
+    '/images/Sophiaonthedock.jpg'
+];
+
+export const foodDish= [
+    '/images/DSC_0377.jpg',
+    '/images/DSC_0366.jpg',
+    '/images/DSC_0403.jpg',
+    '/images/DSC_0421.jpg',
+    '/images/DSC_0424.jpg',
+    '/images/DSC_0427.jpg',
+    '/images/DSC_0442.jpg',
+    '/images/DSC_0469.jpg'
+];
+export const downTown=[
+    '/images/PSX_2001.jpg',
+    '/images/PSX_2002.jpg',
+    '/images/PSX_2003.jpg',
+    '/images/PSX_2004.jpg',
+    '/images/PSX_2005.jpg',
+    '/images/PSX_2006.jpg',
+    '/images/PSX_2007.jpg',
+    '/images/PSX_2008.jpg',
+    '/images/PSX_2009.jpg',
 ];
 const lightboximages= [
-    sunsetonhatteras
+    sunsetHatteras,
+    foodDish,
+    downTown
 
 ];
 export default lightboximages;
--- src/galleryData.js
+++ src/galleryData.js
@@ -1,6 +1,9 @@
 export const galleryData = [
     {
-        imageUrl: '/images/sunsetonhatteras.jpg'
+        imageUrl: '/images/sunsetonhatteras.jpg',
+        imageUrl: '/images/DSC_0377.jpg',
+        imageUrl: '/images/PSX_2001.jpg',
+        imageUrl: '/images/rosegarden.jpg'
     }
 ];
 export default galleryData;
